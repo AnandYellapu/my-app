00f7d39cb037260d5a2daa5072d23bd90891e431
AnandYellapu <anandsaiii1200@gmail.com>
2025-03-28 07:58:29 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import SnakeGame from './components/SnakeGame';
 const App = () => {
   return (
     <div>
-      <h1 style={{ textAlign: 'center', color: 'black', fontSize:'15px' }}>🐍 React Snake Game 🐍</h1>
+      <h1 style={{ textAlign: 'center', color: 'red', fontSize:'15px' }}>🐍 React Snake Game 🐍</h1>
       <SnakeGame />
     </div>
   );
